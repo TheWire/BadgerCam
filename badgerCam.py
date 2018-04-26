f13a13334ceec0b1c453fcd795e6c0ae5932c3ad
TheWire <andrew.ab@gmail.com>
2018-04-27 00:51:26 +0100

    changed switch check to when_held

--- badgerCam.py
+++ badgerCam.py
@@ -1,6 +1,5 @@
 import datetime
 from picamera import PiCamera 
-from time import sleep
 from gpiozero import MotionSensor, LED, Button
 from signal import pause
 
@@ -20,29 +19,24 @@ def video(time, camera, pir):
 
 def motion_detection_handler(camera, pir):
     print("motion detected")
-    video(120, camera, pir)
+    video(10, camera, pir)
 
-def check_switch(switch, led):
-    if switch.is_pressed == True:
-        sleep(1)
-        if switch.is_pressed == True:
-            print("switch off, exiting")
-            led.off()
-            exit()
+def switch_hold_handler(led):
+    print("switch off, exiting")
+    led.off()
+    exit()
 
 def main():
     print("starting")
     onLED = LED(24)
     onLED.on()
-    switch = Button(10)
-    check_switch(switch, onLED)
+    switch = Button(10, hold_time=1)
+    switch.when_held = switch_hold_handler(onLED)
     camera = PiCamera()
     camera.resolution = (1920, 1080)
     pir = MotionSensor(25)
     pir.when_motion = motion_detection_handler(camera, pir)
-    while True:
-        check_switch(switch, onLED)
-        sleep(0.1)
+    pause()
 
 
 if __name__ == '__main__':
