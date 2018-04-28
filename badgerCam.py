de7bb53546d52469b1a0479d2657385589740aac
TheWire <andrew.ab@gmail.com>
2018-04-28 02:32:15 +0100

    6

--- badgerCam.py
+++ badgerCam.py
@@ -3,23 +3,30 @@ from picamera import PiCamera
 from gpiozero import MotionSensor, LED, Button
 from signal import pause
 
-def video(time):
+def video_on(time):
     infredLEDs = LED(5)
     infredLEDs.on()
     now = datetime.datetime.now()
-    camera.start_recording(now.strftime('%Y-%m-%dT%H:%M:%S') + '.h264')
+    if camera.recording == False:
+        print("video on")
+        camera.start_recording(now.strftime('%Y-%m-%dT%H:%M:%S') + '.h264')
+    else 
+        print("Still motion")
     camera.wait_recording(time)
-    while pir.motion_detected == False:
-        print("motion still detected")
-        camera.wait_recording(time)
-    print("video off")
-    camera.stop_recording()
-    infredLEDs.off()
+    
 
+def video_off():
+    if camera.recording == True:
+        print("video off")
+        camera.stop_recording()
+        infredLEDs.off()
+
+def motion_not_detected_handler():
+    video_off()
 
 def motion_detection_handler():
     print("motion detected")
-    video(10)
+    video_on(10)
 
 def switch_hold_handler():
     print("switch off, exiting")
@@ -37,4 +44,5 @@ if __name__== "__main__":
     camera.resolution = (1920, 1080)
     pir = MotionSensor(25)
     pir.when_motion = motion_detection_handler
+    pir.when_no_motion = motion_not_detected_handler
     pause()
