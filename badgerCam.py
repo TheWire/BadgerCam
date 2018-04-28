59cbaefab83519f6acdb3d088aa448f03d28f354
TheWire <andrew.ab@gmail.com>
2018-04-28 01:25:46 +0100

    fix

--- badgerCam.py
+++ badgerCam.py
@@ -31,18 +31,15 @@ def switch_hold_handler():
     onLED.off()
     exit()
 
-
-
-if __name__ == '__main__':
-    print("starting")
-    video_on = False
-    motion = False
-    onLED = LED(24)
-    onLED.on()
-    switch = Button(10)
-    switch.when_held = switch_hold_handler
-    camera = PiCamera()
-    camera.resolution = (1920, 1080)
-    pir = MotionSensor(25)
-    pir.when_motion = motion_detection_handler
-    pause()
+print("starting")
+video_on = False
+motion = False
+onLED = LED(24)
+onLED.on()
+switch = Button(10)
+switch.when_held = switch_hold_handler
+camera = PiCamera()
+camera.resolution = (1920, 1080)
+pir = MotionSensor(25)
+pir.when_motion = motion_detection_handler
+pause()
